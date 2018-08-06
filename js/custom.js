79071c26495c491ed82abb8f86fdcbaa146ad78d
vicnoob <vicnoob.gg@gmail.com>
2018-08-06 22:42:11 +0700

    cv

---
+++ js/custom.js
@@ -0,0 +1,38 @@
+
+jQuery(function () {
+	"use strict";
+    
+    /*global jQuery $*/ 
+	//Slider 
+	$(document).ready(function(){	
+		jQuery('.skillbar').each(function() {
+			jQuery(this).appear(function() {
+				jQuery(this).find('.count-bar').animate({
+					width:jQuery(this).attr('data-percent')
+				},3000);
+				var percent = jQuery(this).attr('data-percent');
+				jQuery(this).find('.count').html('<span>' + percent + '</span>');
+			});
+		});	
+	});
+	(function () {
+        $('a[href*=#]').bind("click", function(e){
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top
+            }, 1000);
+            e.preventDefault();
+        });
+    }());
+	
+	(function () {
+        $('.header').sticky({
+            topSpacing: 0
+        });
+
+        $('body').scrollspy({
+            target: '.navbar-custom',
+            offset: 70
+        })
+    }());
+}());
